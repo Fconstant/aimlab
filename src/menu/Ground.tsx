57b81b4803a27dab1365a55a6c35c7f42d7836c2
Felipe Cavalcante <fcavalcante@nuffsaid.com>
2022-09-14 11:04:57 -0300

    feat: add distort to menu ground

--- src/menu/Ground.tsx
+++ src/menu/Ground.tsx
@@ -1,6 +1,7 @@
 import { usePositionData } from "@app/store/Positions.store";
-import { useLayoutEffect, useRef } from "react";
-import { PointsMaterial } from "three";
+import { MeshDistortMaterial } from "@react-three/drei";
+import { useLayoutEffect, useMemo, useRef } from "react";
+import { PointsMaterial, Vector3 } from "three";
 import { GroundColor } from "./Scene.Colors";
 // import { useVertices } from "./useVertices";
 
@@ -8,6 +9,10 @@ const PLANE_SIZE = [20, 20] as [number, number];
 
 export const SceneGround = () => {
   const { groundPos } = usePositionData();
+  const position = useMemo(
+    () => groundPos.clone().add(new Vector3(0, 1, 0)),
+    [...groundPos.toArray()]
+  );
   const pointsMaterialRef = useRef<PointsMaterial>(null);
 
   useLayoutEffect(() => {
@@ -17,17 +22,17 @@ export const SceneGround = () => {
   }, [pointsMaterialRef.current]);
 
   return (
-    <points position={groundPos} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[...PLANE_SIZE, 64, 64]} />
-      <pointsMaterial
+    <points
+      position={position}
+      rotation={[-Math.PI / 2, 0, 0]}
+      receiveShadow={false}
+    >
+      <planeGeometry args={[...PLANE_SIZE, 96, 96]} />
+      <MeshDistortMaterial
         ref={pointsMaterialRef}
-        args={[
-          {
-            color: GroundColor,
-          },
-        ]}
-        colorWrite
-        size={0.05}
+        color={GroundColor}
+        distort={0.4}
+        speed={2}
       />
     </points>
   );
